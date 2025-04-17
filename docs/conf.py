90bc9a9402bc8b328cbfe198f28bcfde6ebb31a6
pockerman <a.giavaras@gmail.com>
2025-04-17 17:11:56 +0100

    Update version strings

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2025, Alexandros Giavaras'
 author = 'Alexandros Giavaras'
 
 # The full version, including alpha/beta/rc tags
-release = 'v1.9.0'
+release = 'v1.10.6'
 
 
 # -- General configuration ---------------------------------------------------
