5c4627ee270d1acb7d1e3ea59482c9b1e79d1cbb
pockerman <a.giavaras@gmail.com>
2025-04-17 16:58:21 +0100

    Update files for Ray example 2 (#138)

--- examples/ray/ray_example_2/run.sh
+++ examples/ray/ray_example_2/run.sh
@@ -5,6 +5,7 @@ set -e
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE:-$0}")" || exit; pwd)"
 
-bazel --nohome_rc  build //:ray_example_2
+bazel clean
+bazel --nohome_rc  build //:ray_example_2 
 LD_LIBRARY_PATH="$ROOT_DIR/thirdparty/lib" "${ROOT_DIR}"/bazel-bin/ray_example_2
 
